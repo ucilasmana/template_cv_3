dd67a916c76b1f1430560a22fc2f0bd1cdfbe7fa
ucilasmana <uci.lasmana@gmail.com>
2022-08-26 01:43:36 +0700

    first commit

---
+++ src/components/home/HeaderSocials.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+  <div className="home__socials">
+    <a href="https://instagram.com/" className="home__socials-link" target="_blank">
+        <i class='fa-brands fa-instagram'></i>
+    </a>
+    <a href="https://twitter.com/" className="home__socials-link" target="_blank">
+        <i class='fa-brands fa-twitter'></i>
+    </a>
+    <a href="https://behance.com/" className="home__socials-link" target="_blank">
+        <i class='fa-brands fa-behance'></i>
+    </a>
+    <a href="https://dribbble.com/" className="home__socials-link" target="_blank">
+        <i class='fa-brands fa-dribbble'></i>
+    </a>
+    <a href="https://pinterest.com/" className="home__socials-link" target="_blank">
+        <i class='fa-brands fa-pinterest'></i>
+    </a>
+  </div>
+  )
+}
+
+export default HeaderSocials
